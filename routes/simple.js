616eb83851febe492b2726534e59f62185e14414
hsuregan <hsuregan@gmail.com>
2015-11-25 20:48:18 -0800

    return users geographically closest to you

--- routes/simple.js
+++ routes/simple.js
@@ -40,28 +40,21 @@ router.post('/delete', function(req, res){
 //filters long for city, 
 router.post('/closest', function(req, res){
 	//all within one city, then sort closest using geolib
-	console.log("entered closest router");
-	User.find({city:req.body.city}).lean().exec(function(err, usersSameCity) {
+	User.find({city:req.body.city}).limit(10).lean().exec(function(err, usersSameCity) {
 		var ordered = geolib.orderByDistance(req.body, usersSameCity);
-		res.json(ordered);
+		var indexes = [];
+		for(var i = 0; i < ordered.length; i++) {
+			indexes.push(ordered[i]['key']);
+		}
+		var orderedUsers = [];
+		for(var i = 0; i < indexes.length; i++) {
+			orderedUsers.push(usersSameCity[indexes[i]])
+		}
+		console.log(usersSameCity);
+		res.json(orderedUsers);
 		return
 	})
 
-	// 	function(err, usersSameCity){
-	// 	usersSameCity.toJSON();
-
-	// 	JSON.stringify(usersSameCity, function(err, usersSameCityJson){
-
-	// 		console.log(usersSameCityJson);
-
-	// 		var ordered = geolib.orderByDistance(req.body, usersSameCityJson);
-	// 				//res.json({success:'false'});
-
-	// 	});//[{"_id":"56555d83ef2a4210136840bd","username":"Mary","hash":"$2a$10$X5Tx0qT.t/Lk8SMS29uouuOeb1nrHlC.bllkB9CTP6C3J.IUkpXwi","longitude":7.453619,"latitude":51.515,"city":"Austin","__v":0,"matches":[]},,{"_id":"56555d9cef2a4210136840be","username":"Ed","hash":"$2a$10$D4tDwDZ/JZgaTlUSNKaGiOByZ.N3SYga7YC2rgDyAAT0GgmCaTbDe","longitude":13.377722,"latitude":52.516272,"city":"Austin","__v":0,"matches":[]},{"_id":"56555da9ef2a4210136840bf","username":"Chris","hash":"$2a$10$.G5o2keZq.KZPXOqnlXrk.SJNJpn58lN.tObF7pMuY1tAEFExpaxe","longitude":7.45425,"latitude":51.518,"city":"Austin","__v":0,"matches":[]},{"_id":"56555dbeef2a4210136840c0","username":"Brandon","hash":"$2a$10$ke.2VPpVP3LVJcu5Rwxhle68iW4Rr85sAObABZUvA8VCDt9hFV6qK","longitude":-0.119722,"latitude":51.503333,"city":"Austin","__v":0,"matches":[]}]
-	// 	//JSON.stringify(usersSameCity);
-	// 	//var ordered = geolib.orderByDistance(req.body, Users);
-	// 	//res.json(ordered);
-	// })
 })
 
 
